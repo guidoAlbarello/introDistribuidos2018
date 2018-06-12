50733bab1f971a58fc36f641e65f826e831b6862
frenetic <frenetic@ubuntu-1404.vagrantup.com>
2018-06-12 02:57:35 +0000

    custom

---
+++ customPox.sh
@@ -0,0 +1,1 @@
+sudo ~/pox/pox.py samples.pretty_log forwarding.l2_learning openflow.spanning_tree   --no-flood --hold-down  samples.firewall log.level --DEBUG openflow.discovery host_tracker log.level --packet=WARN
